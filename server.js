8277fc694578722fad2a2ce5c90d69ba5ed02a20
gpabloandres <gpabloandres@gmail.com>
2023-04-27 21:21:42 -0300

    Recupera Repo

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var ecstatic = require('ecstatic');
+http.createServer(
+  ecstatic({ root: __dirname + '/public/'})
+).listen(80);
+
+ 
+console.log('Listening on :80');
